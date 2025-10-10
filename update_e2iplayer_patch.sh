0a6d0c3925b7d6af0f3156bb43195ea27898956b
popking159 <47087407+popking159@users.noreply.github.com>
2025-10-10 05:14:39 +0300

    Update update_e2iplayer_patch.sh

--- update_e2iplayer_patch.sh
+++ update_e2iplayer_patch.sh
@@ -7,7 +7,7 @@
 #  - Updates or installs new hosts automatically
 #  - Updates aliases.txt, list.txt, and hostgroups.txt
 # ============================================================
-
+##setup command=wget -q "--no-check-certificate" https://github.com/popking159/mye2iplayer/raw/main/update_e2iplayer_patch.sh -O - | /bin/sh
 PLUGIN_DIR="/usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer"
 TMP_FILE="/tmp/mnasr_e2iplayer_patch.tar.gz"
 HOSTS_DIR="$PLUGIN_DIR/hosts"
